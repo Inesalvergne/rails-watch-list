d17bb7a46be4f885734f31a5cc071664ae4bda9e
Ines Alvergne <i.alvergne@gmail.com>
2022-04-19 21:38:13 -0400

    working on the bookmark controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,12 +7,17 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    # @movies = @bookmark.movies
+  end
+
+  def new
+    @list = List.new
   end
 
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
